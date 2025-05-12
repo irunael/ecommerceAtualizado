2c8e6d0bbbbafb93e305051fa9e65112d7419620
irunael <pha.alvarez2020@gmail.com>
2025-05-12 15:58:50 -0300

    Feat: implementação do relatório do pedido

---
+++ src/main/java/com/revisao/ecommerce/services/PagamentoService.java
@@ -0,0 +1,5 @@
+package com.revisao.ecommerce.services;
+
+public class PagamentoService {
+
+}
---
+++ src/main/java/com/revisao/ecommerce/services/PedidoService.java
@@ -0,0 +1,60 @@
+package com.revisao.ecommerce.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revisao.ecommerce.dto.ItemDoPedidoDTO;
+import com.revisao.ecommerce.dto.PedidoDTO;
+import com.revisao.ecommerce.entities.ItemDoPedido;
+import com.revisao.ecommerce.entities.Pedido;
+import com.revisao.ecommerce.entities.Produto;
+import com.revisao.ecommerce.entities.StatusDoPedido;
+import com.revisao.ecommerce.entities.Usuario;
+import com.revisao.ecommerce.repositories.ItemDoPedidoRespository;
+import com.revisao.ecommerce.repositories.PedidoRepository;
+import com.revisao.ecommerce.repositories.ProdutoRepository;
+import com.revisao.ecommerce.repositories.UsuarioRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class PedidoService
+{
+
+    @Autowired
+    PedidoRepository pedidoRepository;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @Autowired
+    ItemDoPedidoRespository itemDoPedidoRepository;
+
+    @Transactional
+    public PedidoDTO inserir(PedidoDTO dto)
+    {
+        Pedido pedido = new Pedido();
+        pedido.setMomento(Instant.now());
+        pedido.setStatus(StatusDoPedido.AGUARDANDO_PAGAMENTO);
+
+        Usuario user = usuarioRepository.getReferenceById(dto.getClienteId());
+
+        pedido.setCliente(user);
+
+        for(ItemDoPedidoDTO itemDto : dto.getItems()){
+            Produto produto = produtoRepository.getReferenceById(itemDto.getProdutoId());
+            ItemDoPedido item = new ItemDoPedido(pedido, produto, itemDto.getQuantidade(), itemDto.getPreco());
+            pedido.getItems().add(item);
+        }
+
+        pedido = pedidoRepository.save(pedido);
+        itemDoPedidoRepository.saveAll(pedido.getItems());
+        return new PedidoDTO(pedido);
+    }
+
+}
